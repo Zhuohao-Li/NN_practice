cbcee6a1c9d94c8979f8a1480b1dbf0beb1649a8
Edith <edith_lzh@sjtu.edu.cn>
2022-03-29 23:27:49 +0800

    linear_re_0

---
+++ linear_nn/linear_re_form0.py
@@ -0,0 +1,40 @@
+import random
+import torch
+from d2l import torch as d2l
+
+
+def synthetic_data(w, b, num_examples):  #@save
+    """生成 y = Xw + b + 噪声。"""
+    X = torch.normal(0, 1, (num_examples, len(w)))
+    y = torch.matmul(X, w) + b
+    y += torch.normal(0, 0.01, y.shape)
+    return X, y.reshape((-1, 1))
+
+
+true_w = torch.tensor([2, -3.4])
+true_b = 4.2
+features, labels = synthetic_data(true_w, true_b, 1000)
+print('features:', features[0], '\nlabel:', labels[0])
+
+d2l.set_figsize()
+d2l.plt.scatter(features[:, (1)].detach().numpy(), labels.detach().numpy(), 1)
+d2l.plt.savefig('./NN_practice/images/linear2.jpg')
+d2l.plt.show()
+
+
+def data_iter(batch_size, features, labels):
+    num_examples = len(features)
+    indices = list(range(num_examples))
+    # 这些样本是随机读取的，没有特定的顺序
+    random.shuffle(indices)
+    for i in range(0, num_examples, batch_size):
+        batch_indices = torch.tensor(indices[i:min(i +
+                                                   batch_size, num_examples)])
+        yield features[batch_indices], labels[batch_indices]
+
+
+batch_size = 10
+
+for X, y in data_iter(batch_size, features, labels):
+    print(X, '\n', y)
+    break
